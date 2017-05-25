806a05779231aef1ad00fe82eea5679bd97e6a7a
marc <wangchengda1990@gmail.com>
2017-05-25 08:55:16 +0800

    textview自定义属性与onMeasure完善

--- app/src/main/java/marc/com/definitionview/MainActivity.java
+++ app/src/main/java/marc/com/definitionview/MainActivity.java
@@ -2,12 +2,39 @@ package marc.com.definitionview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import marc.com.customview.CView.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+	private TextView mytext;
+
+	private final String TAG = "MainActivity";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		mytext = (TextView)findViewById(R.id.mytext);
+
+		mytext.post(new Runnable() {
+			@Override
+			public void run() {
+				Log.d(TAG, "run: "+mytext.getWidth()+"-"+mytext.getHeight());
+			}
+		});
+
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		Log.d(TAG, "onResume: "+mytext.getWidth()+"-"+mytext.getHeight());
 	}
 }
